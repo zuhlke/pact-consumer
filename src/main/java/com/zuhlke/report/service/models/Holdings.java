2962215cdf6fa29cf0ce4fab44a79e731022f06f
iris-yue-wang <irisluv@gmail.com>
2018-09-14 12:01:59 +0100

    modify: no need to implements Serializable

--- src/main/java/com/zuhlke/report/service/models/Holdings.java
+++ src/main/java/com/zuhlke/report/service/models/Holdings.java
@@ -3,10 +3,9 @@ package com.zuhlke.report.service.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-import java.io.Serializable;
 import java.util.Objects;
 
-public class Holdings implements Serializable {
+public class Holdings {
     @JsonProperty("accountNumber") private String accountNumber;
     @JsonProperty("asOfDate") private String asOfDate;
     @JsonProperty("securityUniqueQual") private String securityUniqueQual;
